4078b4dc251392f538e35072d6089b23eca656a7
Krijn Grimme <krijngrimme@Hotmail.com>
2024-12-23 16:06:46 +0100

    Added gameloop test

--- pkg/blackjack/gameloop.go
+++ pkg/blackjack/gameloop.go
@@ -1,7 +1,6 @@
 package blackjack
 
 import (
-	"fmt"
 	"time"
 )
 
--- pkg/blackjack/gameloop_test.go
+++ pkg/blackjack/gameloop_test.go
@@ -1,17 +1,64 @@
 package blackjack_test
 
 import (
-	"fmt"
+	"slices"
 	"testing"
+	"time"
 
 	"github.com/kraanter/blackjack/pkg/blackjack"
 )
 
 func TestGameLoopSingleRound(t *testing.T) {
-	game := blackjack.CreateGame(blackjack.CreateSettings())
+	settings := blackjack.CreateSettings()
+	settings.DealCardTime = 0 * time.Millisecond
+	game := blackjack.CreateGame(settings)
+	players := make([]*blackjack.Player, 0, 3)
 
-	player1 := game.AddPlayerWithBalance(10)
-	player2 := game.AddPlayerWithBalance(20)
-	player3 := game.AddPlayerWithBalance(30)
+	players = append(players, game.AddPlayerWithBalance(10))
+	players = append(players, game.AddPlayerWithBalance(10))
+	players = append(players, game.AddPlayerWithBalance(10))
+
+	// NOTE: Logging for debugging
+	// game.OnGameUpdate = func(game *blackjack.BlackjackGame) {
+	// fmt.Printf("\n---\ngame_update: %v\n\nplayers:\n", game.GameState)
+	//
+	// for _, player := range players {
+	// 	fmt.Println(player.String())
+	// }
+	//
+	// fmt.Println("Dealer: ", game.Dealer.String())
+	// }
+
+	game.OnPlayerTurn = func(pi blackjack.PlayerId) {
+		game.PlayerHit(pi)
+		game.PlayerStand(pi)
+	}
+
+	go func() {
+		err := game.SetPlayerBet(players[0].PlayerNum, 5)
+		err = game.SetPlayerBet(players[2].PlayerNum, 2)
+		if err != nil {
+			t.Fatalf("Setting player bets went wrong: %v", err.Error())
+		}
+		err = game.SkipPlayerBet(players[1].PlayerNum)
+		if err != nil {
+			println(err.Error())
+			t.Fatalf("Skipping player bet went wrong: %v", err.Error())
+		}
+	}()
+
+	game.Start()
+
+	time.Sleep(1 * time.Millisecond)
+
+	if game.GameState != blackjack.NoState {
+		t.Fatalf("Game should be in no state (%v) after a match is finished, was %v", blackjack.NoState, game.GameState)
+	}
+	if game.Dealer != nil {
+		t.Fatalf("Dealer should not have a hand at the end of the game")
+	}
+	if slices.ContainsFunc(players, func(player *blackjack.Player) bool { return player.Hand != nil }) {
+		t.Fatalf("All players should nto have a hand at the end of the game")
+	}
 
 }
